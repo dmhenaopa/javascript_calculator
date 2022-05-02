33c1ff719316a9ef32032ea2e14dcf1e29bdccc9
dmhenaopa <dimhenaopa@gmail.com>
2022-05-02 17:05:46 -0500

    :construction: Adding some details in design and functionality

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+window.onload = () => {
+    screen = document.getElementById('currentScreen');
+}
+
+let stringNumbers = '';
+
+/* */
+const showNumber = (number) => {
+    screen.innerHTML = number;
+    stringNumbers = stringNumbers + ' ' + number;
+}
+
 const addNumbers = (number1, number2) => {
 
 } 
